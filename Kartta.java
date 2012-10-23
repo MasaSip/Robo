78fd84ee63f8df4b0af3d7786869cf5268ebf43f
Henni Kauppinen <hennikauppinen@gmail.com>
2012-10-23 17:25:04 +0300

    vapaasuuntaisten solmujen tarkistusmetodit luotu Kartta-luokkaan!

--- Kartta.java
+++ Kartta.java
@@ -1,12 +1,19 @@
+
+
 //import java.util.Map;
 import java.util.HashMap;
 import java.awt.Point;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Kartta {
 	private HashMap< Point, Solmu > kartta;
+	private List<Solmu> vapaasuuntaiset;
+	private HermanniRobotti robo;
 	
-	Kartta() {
+	public Kartta() {
 		kartta = new HashMap< Point, Solmu >();
+		this.vapaasuuntaiset = new ArrayList<Solmu>();
 	}
 	
 	public boolean onSolmu(int x, int y) {
@@ -17,8 +24,8 @@ public class Kartta {
 	}
 	
 	public Solmu annaSolmu(int x, int y) {
-		Solmu node = kartta.get(new Point(x,y));
-		return node;
+		Solmu solmu = kartta.get(new Point(x,y));
+		return solmu;
 	}
 	
 	public void lisaaSolmu(int x, int y, Solmu s) {
@@ -35,25 +42,69 @@ public class Kartta {
 			kartta.remove(new Point(x,y));
 	}
 	
-	public static void main(String[] args) {
+	//T€ST€ ALKAA VAPAASUUNTAISET SOLMUT
+	
+	// palauttaa true, jos ymparilla on vapaita suuntia
+	public boolean onVapaa() { // lisaa uusi vapaa
+		// jos ei tunneta tai ei tunneta tai ei tunneta
+		Ilmansuunta i = Ilmansuunta.POHJOINEN;
+		Solmu sijainti = this.annaSolmu(robo.annaX, robo.annaY);
 		
+		do {
+			if (	// ollaanko halutussa suunnassa jo kayty
+					sijainti.annaNaapuriSolmu(i) != null &&
+					// onko halutussa suunnassa seina
+					sijainti.annaEste(i) == Este.VAPAA) {
+				return true;
+			}
+			// suunta ei ollut vapaa, kaannetaan suuntaa
+			else {
+				i = i.vasen();
+			} // else
+		} while (i!=Ilmansuunta.POHJOINEN); // kierratetaan lanteen asti,
+											// koska kaantaa ennen checkia
+		return false;
+	}
+
+	// lisaa tamanhetkinen sijainti vapaiden solmujen listaan
+	public void lisaaVapaa() {
+		Solmu sijainti = this.annaSolmu(robo.annaX, robo.annaY);
+		this.vapaasuuntaiset.add(sijainti);
+	}
+	
+	public void poistaVapaa(Solmu poistettava) {
+		if (poistettava != null)
+			if (vapaasuuntaiset.contains(poistettava))
+					vapaasuuntaiset.remove(poistettava);
+	} // poista pyydetty solmu listasta
+	
+	public Solmu annaViimeisinVapaa() {
+		int koko = vapaasuuntaiset.size();
+		if (koko > 0) { // jos listassa on mitaan
+			
+			Solmu s = vapaasuuntaiset.get(koko-1); // muista viimeisin
+			vapaasuuntaiset.remove(koko-1); // poista viimeisin
+			return s; // palauta viimeisin
+		}
+		return null; // muutoin palauta null
+	} // end of annaViimeisin Vapaa
+	
+	public static void main(String[] args) {
 		Kartta k = new Kartta();
-		Solmu njee = new Solmu();
-		Solmu nhii = new Solmu();
-		k.lisaaSolmu(10, -10);
-		k.lisaaSolmu(10, 5);
+		Solmu njee = new Solmu("jee");
+		Solmu nhii = new Solmu("hii");
+		k.lisaaSolmu(10, -10, njee);
+		k.lisaaSolmu(10, 5, nhii);
 		
 		Solmu n1;
 		n1 = k.annaSolmu(10, -10);
-		System.out.println();
+		System.out.println(n1.annaNimi());
 		
 		n1 = k.annaSolmu(10, 5);
-		System.out.println();
+		System.out.println(n1.annaNimi());
 		n1 = k.annaSolmu(10, 6);
 		if (n1==null)
 			System.out.println("hulabaloo");
 
 	}
 }
-
-
