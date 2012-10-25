5539c5ad71c0ac9df1e498e68fa1f2896c9621b7
Lauri Loikkanen <lauri.loikkanen92@gmail.com>
2012-10-25 13:37:21 +0300

    annaNumero ja asetaNumero metodit tehty

--- Solmu.java
+++ Solmu.java
@@ -6,7 +6,7 @@ public class Solmu {
 	private int x, y;
 	private int numero;
 	private Kartta kartta;
-	
+
 	public void asetaKartalle(Kartta kartta, int x, int y) {
 		this.kartta = kartta;
 		this.x = x;
@@ -14,21 +14,20 @@ public class Solmu {
 	} // asetaKartalle
 
 	// CTOR
-	public Solmu(int x, int y, Kartta kartta, int numero) {
-		
+	public Solmu(int x, int y, Kartta kartta) {
+
 		this.x = x;
 		this.y = y;
 		this.kartta = kartta;
-		this.numero = numero;
-		
+
 		this.esteetSuunnissa = new HashMap<Ilmansuunta, Este>();
-		
+
 		this.asetaEste(Ilmansuunta.POHJOINEN, Este.VAPAA);
 		this.asetaEste(Ilmansuunta.ITA, Este.VAPAA);
 		this.asetaEste(Ilmansuunta.ETELA, Este.VAPAA);
 		this.asetaEste(Ilmansuunta.LANSI, Este.VAPAA);
 	} // end of CTOR
-	
+
 
 	/*
 	 * Anna Solmun Este-status pyydetylle Solmulle
@@ -36,7 +35,7 @@ public class Solmu {
 	public Este annaEste(Ilmansuunta i) {
 		return this.esteetSuunnissa.get(i);
 	} // end of annaEsteet
-	
+
 
 	/*
 	 * Aseta Solmun pyydetylle suunnalle Este-status
@@ -44,21 +43,21 @@ public class Solmu {
 	public void asetaEste(Ilmansuunta i, Este este) {
 		this.esteetSuunnissa.put(i, este);
 	} // end of asetaEste
-	
-	
+
+
 	/*
 	 * Antaa naapurisolmun pyydetysta suunnasta, tama on null jos ei ole
 	 */
 	public Solmu annaNaapuriSolmu(Ilmansuunta suunta) {
-		
+
 		// onko maailmassa
 		if (kartta!=null) {
-		
+
 			// apukoordinaatit, rajoissa [-1,1]
-			
+
 			int x = 0;
 			int y = 0;
-			
+
 			// aseta apukoordinaatit osoittamaan naapuriruudun suuntaan
 			if (suunta!=null)
 				switch (suunta) {
@@ -81,18 +80,18 @@ public class Solmu {
 			/*
 			 * Palauta Solmu halutusta kohdasta
 			 * joka on kohdassa {oma sijainti + apukoordinaatit} 
-			*/
+			 */
 			return kartta.annaSolmu(this.y+y, this.x+x);
-			
+
 		} // kartta on olemassa
 		return null;
 	} // end of annaNaapuriSolmu
 
-	/*
-	 * TODO
-	 * 
-	 * annaNumero(), palauttaa numeron
-	 */
-
-
+	public int annaNumero(){
+		return this.numero;
+	}
+	
+	public void asetaNumero(int numero){
+		this.numero = numero;
+	}
 }
