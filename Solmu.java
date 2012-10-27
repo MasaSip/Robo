c33a965cb8a2b8e5310628cebff1522051d08259
Ilona Luukko <ilona.luukko@luukku.com>
2012-10-27 11:06:04 +0300

    Solmun annaNaapurisolmusta koodi poistettu
    ja muutettu käyttämään Kartan annaViereisenKoordinaatit
    toiminnallisuutta

--- Solmu.java
+++ Solmu.java
@@ -58,43 +58,7 @@ public class Solmu {
 	 * Antaa naapurisolmun pyydetysta suunnasta, tama on null jos ei ole
 	 */
 	public Solmu annaNaapuriSolmu(Ilmansuunta suunta) {
-
-		// onko maailmassa
-		if (kartta!=null) {
-
-			// apukoordinaatit, rajoissa [-1,1]
-
-			int x = 0;
-			int y = 0;
-
-			// aseta apukoordinaatit osoittamaan naapuriruudun suuntaan
-			if (suunta!=null)
-				switch (suunta) {
-				case POHJOINEN:
-					y = 1;
-					break;
-				case LANSI:
-					x = -1;
-					break;
-				case ETELA:
-					y = -1;
-					break;
-				case ITA:
-					x = 1;
-					break;
-				default:
-					// ilmansuunta ei ole sopiva
-					return null;
-				} // end of switch
-			/*
-			 * Palauta Solmu halutusta kohdasta
-			 * joka on kohdassa {oma sijainti + apukoordinaatit} 
-			 */
-			return kartta.annaSolmu(this.y+y, this.x+x);
-
-		} // kartta on olemassa
-		return null;
-	} // end of annaNaapuriSolmu
+	return Kartta.annaViereisenKoordinaatit(int x, int y, Ilmansuunta i);
 
 	public int annaAskelNumero(){
 		return this.numero;
